dcb27a5798a5d53c657e7c81cb7af738614ac93b
Filippoinf <123358532+Filippoinf@users.noreply.github.com>
2023-02-22 13:26:17 +0100

    Add files via upload

---
+++ area pyt..py
@@ -0,0 +1,9 @@
+Python 3.11.2 (tags/v3.11.2:878ead1, Feb  7 2023, 16:38:35) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> base = int(input("inserisci la base del quadrato"))
+inserisci la base del quadrato10
+>>> altezza = int(input("inserisci l'altezza del quadrato"))
+inserisci l'altezza del quadrato10
+>>> area = base * altezza / 2
+>>> print(area)
+50.0
